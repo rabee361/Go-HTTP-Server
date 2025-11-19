b6c35af10e6d3353e82717c251174957d9f85f4d
rabee361 <rha60540@gmail.com>
2025-11-19 14:16:50 +0300

    fix

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	// "encoding/json"
+)
+
+
+// type Person struct {
+// 	name string
+// 	age int
+// 	email string
+// }
+
+// func newPerson(name string, ) *Person {
+// 	p := Person{name: "Rabee", age: 20, email:"rabee@emai.cpm"}
+// 	return &p
+// }
+
+func main() {
+	// rabee := Person{age: 20};
+	// fmt.Print(rabee.age)
+
+	x := 'g'
+	fmt.Println(x)
+}
