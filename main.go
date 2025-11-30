207d8f7972be230f9e6ff164d1df079805965ba6
rabee361 <rha60540@gmail.com>
2025-11-30 16:30:50 +0300

    init

--- main.go
+++ main.go
@@ -2,25 +2,49 @@ package main
 
 import (
 	"fmt"
-	// "encoding/json"
+	"net"
+	"os"
+	"bufio"
 )
 
+const (
+	SERVER_HOST = "localhost"
+	SERVER_PORT = "9988"
+	SERVER_TYPE = "tcp"
+)
 
-// type Person struct {
-// 	name string
-// 	age int
-// 	email string
-// }
+type Request struct {
 
-// func newPerson(name string, ) *Person {
-// 	p := Person{name: "Rabee", age: 20, email:"rabee@emai.cpm"}
-// 	return &p
-// }
+}
 
 func main() {
-	// rabee := Person{age: 20};
-	// fmt.Print(rabee.age)
+	
+	server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	fmt.Print("Server runnin on port: " , SERVER_PORT)
+	if err != nil {
+		fmt.Println("Error dialing", err.Error()) // if connection failed
+		os.Exit(1)
+	}	
 
-	x := 'g'
-	fmt.Println(x)
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting", err.Error()) // if accept failed
+			os.Exit(1)
+		}
+		fmt.Println("client connected")
+		go processClient(conn)
+	}
+
+}
+
+ 
+func processClient(conn net.Conn) { 
+	reader := bufio.NewReader(conn)
+	msg_len , err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading", err.Error())
+	}
+	fmt.Println("Recieved", string(msg_len))
+	// conn.Close()
 }
